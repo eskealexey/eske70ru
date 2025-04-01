c1ea14d670887b237a7b8556ef47dbbfac0edb6d
alexey <eskealexey@mail.ru>
2025-04-01 22:10:54 +0300

    модернизация админки

--- eske70ru/programs/admin.py
+++ eske70ru/programs/admin.py
@@ -3,8 +3,21 @@ from filebrowser.sites import site
 from .models import *
 
 
+@admin.register(Program)
+class ProgramAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'languages', 'platforms', 'short_description', 'date', 'is_active']
+    list_display_links = ['id', 'name', 'date']
+    search_fields = ['name']
+    list_editable = ('is_active',)
+
+
+@admin.register(SoftDownload)
+class SoftDownloadAdmin(admin.ModelAdmin):
+    list_display = ['id', 'program_id', 'ip_remote', 'date', 'count']
+    list_display_links = ['program_id']
+
+
 # Register your models here.
 admin.site.register(Languages)
-admin.site.register(Program)
 admin.site.register(Platforms)
-admin.site.register(SoftDownload)
+# admin.site.register(SoftDownload)
--- eske70ru/projects/admin.py
+++ eske70ru/projects/admin.py
@@ -3,5 +3,19 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(Project)
-admin.site.register(Comment)
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('id','title','category', 'status', 'views')
+    list_display_links = ('id', 'title')
+    list_editable = ('status',)
+
+
+# @admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('id','project', 'author', 'parent', 'text','is_active', 'created_at')
+    list_display_links = ('id', 'project','author', 'parent', 'text')
+
+
+
+# admin.site.register(Project)
+admin.site.register(Comment, CommentAdmin)
